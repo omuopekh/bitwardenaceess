bafe40e13e73d499934aaad0e0b4eeb7a0b4bb82
omuopekh <romuopekh@outlook.com>
2026-03-25 17:11:08 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,30 @@
+project = 'bitwardenaccesshelp'
+author = 'bitwardenaccesshelp'
+release = '1.0'
+
+# Extensions
+extensions = [
+    'sphinx_sitemap',
+]
+
+# Paths
+templates_path = ['_templates']
+exclude_patterns = []
+
+# Theme
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+# Custom JS & Favicon
+html_js_files = ['chatbot.js']  # chatbot widget
+html_favicon = '_static/favicon.png'
+
+# Bing search verification
+html_context = {
+    'bing_verification_code': '59FDF959BB464F16C29E6DC18623CEF1'
+}
+
+# Base URL for sitemap
+html_baseurl = 'https://bitwardenaccesshelp.readthedocs.io/en/latest/'
+
+
